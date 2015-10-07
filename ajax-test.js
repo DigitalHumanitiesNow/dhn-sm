6e7a1411b8be9f80fa9198361702dd879e14aa3d
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2015-10-07 14:38:22 -0400

    adds log file and functions.php file.

--- ajax-test.js
+++ ajax-test.js
@@ -1,13 +1,11 @@
-jQuery(document).ready( function($) {
-	$("#test").click( function() {
-		var data = {
-			action: 'test_response',
-                        post_var: 'this will be echoed back'
-		};
-		// the_ajax_script.ajaxurl is a variable that will contain the url to the ajax processing file
-	 	$.post(the_ajax_script.ajaxurl, data, function(response) {
-			alert(response);
-	 	});
-	 	return false;
-	});
-});
+function myAjax() {
+      $.ajax({
+           type: "POST",
+           url: 'http://local.wordpress.dev/wp-content/plugins/dhn-sm/index.php',
+           data:{action:'call_this'},
+           success:function(html) {
+             alert(html);
+           }
+
+      });
+ }
