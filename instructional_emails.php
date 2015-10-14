f8b229acc2b5dd96f4ab9a37004c7e33f731265a
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2015-10-14 13:23:24 -0400

    bcc test

--- instructional_emails.php
+++ instructional_emails.php
@@ -68,7 +68,7 @@ function send_instructions() {
 					if (in_array($next_week, $checkbox)){
 						$userinfo = get_userdata($user->ID);
 						$userdetails .= '<tr><td>' . $userinfo->user_login . '</td><td>' . $userinfo->user_email . '</td></tr>';
-						$headers[] = "Bcc: '" . $userinfo->user_email . "' ";
+						$headers[] = "Bcc: '" . $userinfo->user_email . "', ";
 						$emails_nw[] = $userinfo->user_email;
 					}
 			} //end foreach
